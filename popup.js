06970370fe4227600ca3ac980cf664afefc04424
Adam <adam@the42ndestate.com>
2014-09-09 17:27:00 -0400

    Add checkboxes, use AJAX calls hack to figure out content type. Show image if image. New icons

--- popup.js
+++ popup.js
@@ -25,24 +25,30 @@ var saveTabsAs = {
 	createForm: function (tabs) {
 		var numTabs = document.getElementById('numTabs');
 		numTabs.innerHTML = "<p> This window has " + tabs.length + " tabs.";
-//		form.insertBefore(numTabs);
 
 		var resources = document.getElementById('resources');
 
 		tabs.forEach(function(tab) {
+			var contentType = saveTabsAs.getContentType(tab.url, function(response){
+				this.response = response;
+			});
+			
 			var input = document.createElement('input');
 				input.type = "checkbox";
 				input.id = 'tab-' + tab.index;
 				input.name = 'tabs' + tab.index;
-				input.title = tab.title;
+				input.title = tab.title + this.response;
 				input.value = tab.url;
 				input.checked = "checked";
 
 			var shortDescription = tab.title.substring(0,10);
 
 			var label = document.createElement('label');
-//				label.innerText = shortDescription;
-				label.innerText = tab.title;
+//              label.innerText = shortDescription;
+				label.innerHTML = '<p>Title: ' + tab.title + '</p><p> Type: ' + this.response + '</p>';
+				if(this.response.split("/").shift() == 'image'){
+					label.innerHTML += '<img src=' + tab.url + '/>';
+				}
 				label.appendChild(input);
 
 			var li = document.createElement('li');
@@ -68,17 +74,33 @@ var saveTabsAs = {
 	},
 
 	getTabStatus: function(tabs){
-		var allTabs = document.getElementById('tabs-ALL');
-
-		if(!allTabs.checked){
 			var selectedTabs = this.getSelectedTabs(tabs.length);
 			this.downloadUrls(selectedTabs);
-			return;
-		}
+	},
 
-		var urls = this.getUrls(tabs);
-		this.downloadUrls(urls);
-		return;
+	getContentType: function(url, callback){
+		var xhr = new XMLHttpRequest();
+		var cType = "";
+			xhr.open("GET", url, false);
+			xhr.onload =  function(e) {
+				if (xhr.readyState == 4) {
+					if(xhr.status === 200) {
+						callback(xhr.getResponseHeader("Content-Type"));
+					}
+					else{
+						console.error(xhr.statusText);
+					}
+				}
+			};
+			xhr.onerror = function (e) {
+				console.error(xhr.statusText);
+			};
+			xhr.send();
+
+	},
+
+	getExtension: function(url){
+		return url.split('.').pop();
 	},
 
 	watchSubmit: function (tabs) {
