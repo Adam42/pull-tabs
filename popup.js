6c18433ca353f449b8bdb85e55477f8241a2b956
Adam <adam@the42ndestate.com>
2014-09-11 22:58:34 -0400

    Added a dev browser

--- popup.js
+++ popup.js
@@ -195,19 +195,42 @@ document.addEventListener('DOMContentLoaded', function () {
 var Browser = {
 
     getTabs: function (info) {
-        if (chrome) { 
+        if (!(typeof chrome ==='undefined')) { 
             var browser = STSChrome;
+        }
+        else{
+            var browser = DevBrowse;
         }
 
         browser.getTabs(info);
     },
 
     downloadUrls: function (urls) {
-        if (chrome) { 
+        if (!(typeof chrome ==='undefined')) { 
             var browser = STSChrome;
+        }
+        else{
+            var browser = DevBrowse;
         }
 
         browser.downloadUrls(urls)
+    },
+}
+
+var DevBrowse = {
+    downloadUrls: function (urls) {
+        urls.forEach(function(url){
+            var file = {
+                "url": url,
+                "method": "GET"
+            };
+            console.log('Dev downloaded ' + file);
+        });
+    },
+
+    getTabs: function ( info ) {
+        var devtabs = '[{"active":false,"height":779,"highlighted":false,"id":71,"incognito":false,"index":0,"pinned":false,"selected":false,"status":"complete","title":"Dot Boston: Apple, Bicycles, Boston, Dot and Web Media","url":"http://adamp.com/","width":1440,"windowId":68},{"active":false,"height":779,"highlighted":false,"id":83,"incognito":false,"index":1,"pinned":false,"selected":false,"status":"complete","title":"Is It Boston? Find out if your area is part of Boston.","url":"http://isitboston.com/","width":1440,"windowId":68},{"active":false,"height":779,"highlighted":false,"id":85,"incognito":false,"index":2,"pinned":false,"selected":false,"status":"complete","title":"amiacylon.com","url":"http://amiacylon.com/","width":1440,"windowId":68},{"active":true,"height":779,"highlighted":true,"id":91,"incognito":false,"index":3,"pinned":false,"selected":true,"status":"complete","title":"3571814663_5c742efc65_b.jpg (1024×768)","url":"https://c4.staticflickr.com/4/3322/3571814663_5c742efc65_b.jpg","width":1440,"windowId":68}]';
+        var tabs = saveTabsAs.createForm(JSON.parse(devtabs));
     },
 }
 
