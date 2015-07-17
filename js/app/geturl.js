b9c2b7d928afe78f1a5b2db3db2d1535df696ca8
Adam <adam@the42ndestate.com>
2015-07-17 01:07:13 -0400

    Consolidate xhr actions into one try clause

--- js/app/geturl.js
+++ js/app/geturl.js
@@ -1,49 +1,35 @@
 function getURL( url ) {
-    var xhr = new XMLHttpRequest();
+
     var file = 'file:///Users/adam/Dropbox/Sites/pullTabs/config.json';
 
-    console.log(xhr);
+    var xhr;
+
     if(typeof(XMLHttpRequest) === 'undefined'){
         //we're in node.js for testing
-        console.log('node.js for testing');
         console.log("Using node.js xhr");
-        var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;   
-        try{   
-            xhr = new XMLHttpRequest();
-            xhr.open('GET', file, true);
-            xhr.setRequestHeader("Content-type: application/json");
-
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == "200") {
-                    //console.log(xhr.responseText);
-                    callback.apply(xhr.responseText);
-                }
-            };
-            xhr.send(null);
-        }
-        catch (e) {
-            console.log(e);
-        }
+        var NODEXMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+        xhr = new NODEXMLHttpRequest();
     }
     else{
         //we're in a browser!
-        try{
-            xhr = new XMLHttpRequest();
-            xhr.overrideMimeType("application/json");
-            xhr.open('GET', file, false);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == "200") {
-                    callback.apply(xhr.responseText);
-                }
-            };
-            xhr.send(null);
-        }
-        catch (e) {
-            console.log(e);
-        }
+        xhr = new XMLHttpRequest();
     }
+    try{
+        xhr.open('GET', file, false);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.overrideMimeType("application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == "200") {
+                callback.apply(xhr.responseText);
+            }
+        };
+        xhr.send(null);
+    }
+    catch (e) {
+        console.log(e);
+    }
+
 
     return;
 }
-
-getURL(url);
