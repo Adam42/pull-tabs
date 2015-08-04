69a46d076f0d765454ca0a85dc98e016ea432bfb
Adam <adam@the42ndestate.com>
2015-08-03 20:35:34 -0400

    Add a sample config and gitignore the real one

---
+++ js/app/config-sample.js
@@ -0,0 +1,26 @@
+//Change this filename to config.js and fill in the details below
+var pullTabsApp = pullTabsApp || {};
+pullTabsApp.Config = pullTabsApp.Config || (function () {
+
+    var setup = {
+        "credentials" : {
+            //get an API key from getpocket.com and place it here
+            "consumer_key": "YOUR CONSUMER KEY HERE"
+        },
+
+        "configuration" : {
+            //DEVELOPMENT is the other mode
+            //though it is not very fleshed out
+            //and this repo doesn't include node's XMLHttpRequest
+            //...you should probably just leave this as is
+            "mode": "PRODUCTION"
+        },
+
+        "app": {
+            "name": "Pull Tabs"
+        }
+    };
+
+    return setup;
+
+}(pullTabsApp));
