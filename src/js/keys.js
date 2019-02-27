85257677c8cc394a1a0eea021f03f2085f36a54e
Adam Pieniazek <adam@the42ndestate.com>
2019-02-27 00:56:56 -0500

    Document layout, autoclose and defaultServices

--- src/js/keys.js
+++ src/js/keys.js
@@ -4,11 +4,19 @@ export const keys = [];
 
 keys.preferences = [];
 
+/**
+ * Layout preference key, determines which views to display in popup
+ * @type {Object}
+ */
 keys.preferences.layout = {
   simple: "true",
   advanced: "false"
 };
 
+/**
+ * User preference key for whether to close tabs after a successful action
+ * @type {Object}
+ */
 keys.preferences.autoClose = {
   autoCloseTabs: false
 };
@@ -23,6 +31,10 @@ keys.preferences.tabActions = actions;
 
 keys.preferences.tabOptions = ["enabled", "disabled"];
 
+/**
+ * Creates a collection of services based on the action
+ * string and sets a default state for each service
+ */
 function setDefaultServices() {
   for (let action in actions) {
     let name = actions[action];
